17a10a9f5096adc12e3a6500f70a0c7c496e1528
BrandonERivera <brandonellis.rivera@gmail.com>
2023-12-11 19:03:54 -0500

    added to comments

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,6 +27,7 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
+//done
 registerRoute(
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,12 +2,14 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
+//done
 window.addEventListener('beforeinstallprompt', (event) => {
     window.deferredPrompt = event;
     butInstall.classList.toggle('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
+//done
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
     if (!promptEvent) {
@@ -19,6 +21,7 @@ butInstall.addEventListener('click', async () => {
 });
 
 // TODO: Add an handler for the `appinstalled` event
+//done
 window.addEventListener('appinstalled', (event) => {
     window.deferredPrompt = null;
 });
